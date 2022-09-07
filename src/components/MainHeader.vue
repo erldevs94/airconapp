fe2689f690e13a56d651b3a7ba72dba208643f4d
Erlou Cabilan <erlou.c@vonex.com.au>
2022-09-08 06:44:52 +0800

    adding logo and schema

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -4,14 +4,17 @@
       <q-toolbar>
         <q-toolbar-title
           ><router-link to="/" class="logoText">
-            <span class="q-pa-sm text-primary text-bold text-no-wrap logoText"
-              >Clean Aire
-              <span class="material-icons text-h4" glossy> ac_unit </span></span
-            ></router-link
-          ></q-toolbar-title
+            <img
+              src="../assets/imgs/logo.png"
+              id="logo"
+              alt="Air conditioning services"
+            />
+            <span class="text-subtitle2 text-grey-9">
+              Airconditioning Services</span
+            >
+          </router-link></q-toolbar-title
         >
 
-            
         <q-btn flat icon="menu" label="Menu" class="text-primary">
           <q-menu
             style="width: 10rem"
@@ -21,10 +24,13 @@
           >
             <q-list class="bg-primary">
               <q-item v-for="(route, index) in routes" :key="index">
-                <q-item-section >
-                  <router-link style="text-decoration: none;" class="text-bold text-blue-1" :to="route.path">{{
-                    route.label
-                  }}</router-link></q-item-section
+                <q-item-section>
+                  <router-link
+                    style="text-decoration: none"
+                    class="text-bold text-blue-1"
+                    :to="route.path"
+                    >{{ route.label }}</router-link
+                  ></q-item-section
                 >
               </q-item>
             </q-list>
@@ -33,8 +39,26 @@
       </q-toolbar>
     </q-header>
     <q-footer class="bg-blue-grey-10" elevated>
+      <div itemscope itemtype="https://schema.org/HVACBusiness">
+        <meta itemprop="name" content="Clean Aire" />
+        <div
+          itemprop="address"
+          itemscope
+          itemtype="https://schema.org/PostalAddress"
+        >
+          <meta itemprop="streetAddress" content="Casili" />
+          <meta itemprop="addressLocality" content="Consolacion, Cebu" />
+          <meta itemprop="addressRegion" content="VII" />
+          <meta itemprop="postalCode" content="6001" />
+        </div>
+        <meta content="09918625382" itemprop="telephone" />
+        <meta content="louwellcabilan08@gmail.com" itemprop="email" />
+        <meta itemprop="url" content="https://clean-aire.netlify.app/" />
+        <meta itemprop="openingHours" content="Mo-Sa 8:00-6:00" />
+      </div>
       <q-toolbar>
         <q-toolbar-title>Clean Aire</q-toolbar-title>
+
         <small>Copyright <q-icon name="copyright"></q-icon> 2022</small>
       </q-toolbar>
     </q-footer>
@@ -50,13 +74,20 @@ const routes = ref([
 </script>
 
 <style scoped>
-.logoText {
-  text-decoration: none;
+#logo {
+  max-width: 15rem;
+  width: 100%;
 }
 
 #main_header_stick {
   position: sticky;
   top: 0;
   z-index: 999;
+}
+
+.text-subtitle2 {
+  position: absolute;
+  left: 45px;
+  bottom: 0;
 }
 </style>
